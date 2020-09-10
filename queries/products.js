53a7e7a0385271c71be3067d4c846ed8a58bd7b1
Muzyar Rad <muzyar.rad@gmail.com>
2020-09-10 15:49:15 -0700

    created the routes folder and routes file for constructing startup folder

--- queries/products.js
+++ queries/products.js
@@ -1,9 +1,26 @@
 const express = require("express");
-const {db}= require("../db")
+const { db } = require("../db");
 const router = express.Router();
 
-
-
-router.get("/", (req,res) => {
-    const res = 
-})
+router.get("/modifycolumn", (req, res) => {
+  const sql =
+    'ALTER TABLE "products" MODIFY COLUMN productID int auto_increment primary key';
+  db.query(sql, (err, res) => {
+    if (err) console.log(err);
+    console.log(res);
+  });
+});
+router.get("/getproducts", (req, res) => {
+  const sql =
+    "INSERT INTO products (name, description, imagePath, price) VALUES ?";
+  const values = [
+    ["Corsair1", "This is an awesome desktop", "/local/c", "$3,999.00"],
+    ["Corsair3", "This is an awesome desktop 2", "/local/a", "$2,999.00"],
+    ["Aurora10", "This is an awesome desktop 3", "/local/2", "$5,999.00"],
+    ["Aurora11", "This is an awesome desktop 4", "/local/d", "$6,999.00"],
+  ];
+  db.query(sql, [values], (err, res) => {
+    if (err) throw err;
+    console.log(res);
+  });
+});
