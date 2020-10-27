8bc33f4619c2f55e336ab58dacc1568797174647
Muzyar Rad <muzyar.rad@gmail.com>
2020-10-26 19:15:20 -0700

    added navBar component and added it to App component

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import NavBar from './components/navBar';
 function App() {
-  return <div className="App"></div>;
+  return <div>
+    <NavBar />
+  </div>;
 }
 
 export default App;
---
+++ frontEnd/src/components/navBar.js
@@ -0,0 +1,9 @@
+import React, {useState,useEffect} from 'react'
+
+
+
+const NavBar = () => {
+    return <div></div>;
+}
+ 
+export default NavBar;
