96cc856c11ce5f0d52e98d2fd31db2b45c059719
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-04 12:28:42 -0800

    nav almost created

--- frontEnd/src/components/navBar.js
+++ frontEnd/src/components/navBar.js
@@ -5,45 +5,19 @@ const NavBar = () => {
   return (
     <div className="container-fluid">
       <div className="topNav mb-3">GameBox</div>
-      <nav className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="#">
-          Navbar
+      <nav class="nav nav-pills nav-justified">
+        <a class="nav-item nav-link" href="#">
+          Active
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">
-                Disabled
-              </a>
-            </li>
-          </ul>
-        </div>
+        <a class="nav-item nav-link" href="#">
+          Link
+        </a>
+        <a class="nav-item nav-link" href="#">
+          Link
+        </a>
+        <a class="nav-item nav-link" href="#">
+          Disabled
+        </a>
       </nav>
     </div>
   );
