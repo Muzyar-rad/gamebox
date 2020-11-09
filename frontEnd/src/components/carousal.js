eef206004ea2add307f6127f82e90a2a46b1ef74
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-08 18:50:51 -0800

    added images for testing carousal

--- frontEnd/src/components/carousal.js
+++ frontEnd/src/components/carousal.js
@@ -1,31 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Carousal = () => {
   return (
     <div
       id="carouselExampleFade"
-      class="carousel slide carousel-fade"
+      className="carousel slide carousel-fade"
       data-ride="carousel"
     >
-      <div class="carousel-inner"></div>
-      <a
-        class="carousel-control-prev"
-        href="#carouselExampleFade"
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img
+            src={"../assets/baelen.jpg"}
+            className="d-block w-100"
+            alt="Number 1"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src={"../assets/2.jpg"}
+            className="d-block w-100"
+            alt="Number 2"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            src={"../assets/3.jpg"}
+            className="d-block w-100"
+            alt="Number 3"
+          />
+        </div>
+      </div>
+      <Link
+        className="carousel-control-prev"
+        to="#carouselExampleFade"
         role="button"
         data-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a
-        class="carousel-control-next"
-        href="#carouselExampleFade"
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
+      </Link>
+      <Link
+        className="carousel-control-next"
+        to="#carouselExampleFade"
         role="button"
         data-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
+      </Link>
     </div>
   );
 };
--- frontEnd/src/components/navBar.js
+++ frontEnd/src/components/navBar.js
@@ -9,20 +9,20 @@ const NavBar = () => {
         <div className="ml-3">GameBox</div>
         <div className="d-flex justify-content-between m-5 pt-3 cart">
           <p className="badge">0</p>
-          <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+          <i className="fa fa-shopping-cart" aria-hidden="true"></i>
         </div>
       </div>
-      <nav class="nav nav-pills nav-justified">
-        <Link class="nav-item nav-link" to="#">
+      <nav className="nav nav-pills nav-justified">
+        <Link className="nav-item nav-link" to="#">
           DESKTOPS
         </Link>
-        <Link class="nav-item nav-link" to="#">
+        <Link className="nav-item nav-link" to="#">
           LAPTOPS
         </Link>
-        <Link class="nav-item nav-link" to="#">
+        <Link className="nav-item nav-link" to="#">
           GEARSHOP
         </Link>
-        <Link class="nav-item nav-link" to="#">
+        <Link className="nav-item nav-link" to="#">
           GIFT CARDS
         </Link>
       </nav>
