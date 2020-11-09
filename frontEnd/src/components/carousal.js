97777cd9db94e1dcfa34e2aa170256fab21f1c5d
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-08 18:14:19 -0800

    added sample images for testing carousal

--- frontEnd/src/components/carousal.js
+++ frontEnd/src/components/carousal.js
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Carousal = () => {
-  return <div></div>;
+  return (
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <ol class="carousel-indicators">
+        <li
+          data-target="#carouselExampleIndicators"
+          data-slide-to="0"
+          class="active"
+        ></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+    </div>
+  );
 };
 
 export default Carousal;
