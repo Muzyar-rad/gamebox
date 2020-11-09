657c5ae0a02037d83349e6adaae77e517a0104d3
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-08 18:18:32 -0800

    added parts of carousal

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/navBar";
+import Carousal from "./components/carousal";
 import "./App.css";
 function App() {
   return (
     <div className="main mh-100">
       <NavBar />
+      <Carousal />
     </div>
   );
 }
