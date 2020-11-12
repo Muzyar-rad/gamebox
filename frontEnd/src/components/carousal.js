319edff45192832db59a7942f9b499d93679152e
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-12 15:31:18 -0800

    working on carousal images

--- frontEnd/src/components/carousal.js
+++ frontEnd/src/components/carousal.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../css/carousal.css";
 import img1 from "../assets/2.jpg";
 import img2 from "../assets/3.png";
 import img3 from "../assets/baelen.jpg";
 
 const Carousal = () => {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <div
         id="carouselExampleFade"
         className="carousel slide carousel-fade"
@@ -14,17 +15,13 @@ const Carousal = () => {
       >
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img
-              alt="Number 1"
-              src={img1}
-              className="d-block w-100 img-fluid"
-            />
+            <img alt="Number 1" src={img1} className="d-block w-50" />
           </div>
           <div className="carousel-item">
-            <img alt="Number 2" src={img2} className="d-block w-100" />
+            <img alt="Number 2" src={img2} className="d-block w-50" />
           </div>
           <div className="carousel-item">
-            <img src={img3} className="d-block w-100" alt="Number 3" />
+            <img src={img3} className="d-block w-50" alt="Number 3" />
           </div>
         </div>
         <a
