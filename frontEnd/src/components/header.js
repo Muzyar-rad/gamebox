e149305df2188b315ccbe928cab308a8f18fc4e3
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-16 18:29:40 -0800

    almost finished with navbar

--- frontEnd/src/components/header.js
+++ frontEnd/src/components/header.js
@@ -4,13 +4,14 @@ import "../css/header.css";
 
 const Header = () => {
   return (
-    <div className="bg-dark text-light d-flex align-items-center justify-content-between mb-3">
-      <div className="ml-3">GameBox</div>
-      <div className="d-flex justify-content-between m-5 pt-3 cart">
-        <p>0 items</p>
-        <i className="fa fa-shopping-cart" aria-hidden="true"></i>
-      </div>
-    </div>
+    // <div className="bg-dark text-light d-flex align-items-center justify-content-between mb-3">
+    //   <div className="ml-3">GameBox</div>
+    //   <div className="d-flex justify-content-between m-5 pt-3 cart">
+    //     <p>0 items</p>
+    //     <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+    //   </div>
+    // </div>
+
     // <nav className="nav nav-pills nav-justified">
     //   <Link className="nav-item nav-link" to="#">
     //     DESKTOPS
@@ -22,6 +23,16 @@ const Header = () => {
     //     GEARSHOP
     //   </Link>
     // </nav>
+
+    <nav className="navbar navbar-dark bg-dark">
+      <Link className="navbar-brand ml-5" to="/">
+        GameBox Center
+      </Link>
+      <div className="d-flex justify-content-between align-items-baseline text-light mt-3 mr-5">
+        <p>0 items</p>
+        <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+      </div>
+    </nav>
   );
 };
 
