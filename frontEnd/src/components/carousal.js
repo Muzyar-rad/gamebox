f238498bcb34669826714d0915978b5c6a059055
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-19 16:57:30 -0800

    images uploaded to carousal

--- frontEnd/src/components/carousal.js
+++ frontEnd/src/components/carousal.js
@@ -1,61 +1,52 @@
 import React from "react";
+import { img1 } from "../assets/img/gaming_laptop_1.webp";
+import { img2 } from "../assets/img/gaming_laptop_2.jpg";
+import { img3 } from "../assets/img/gaming_laptop_3.webp";
 
 const Carousal = () => {
   return (
     <div
       id="carouselExampleIndicators"
-      class="carousel slide"
+      className="carousel slide"
       data-ride="carousel"
     >
-      <ol class="carousel-indicators">
+      <ol className="carousel-indicators">
         <li
           data-target="#carouselExampleIndicators"
           data-slide-to="0"
-          class="active"
+          className="active"
         ></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img
-            class="d-block w-100"
-            src=".../800x400?auto=yes&bg=777&fg=555&text=First slide"
-            alt="First slide"
-          />
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img className="d-block w-100" src={img1} alt="First slide" />
         </div>
-        <div class="carousel-item">
-          <img
-            class="d-block w-100"
-            src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide"
-            alt="Second slide"
-          />
+        <div className="carousel-item">
+          <img className="d-block w-100" src={img2} alt="Second slide" />
         </div>
-        <div class="carousel-item">
-          <img
-            class="d-block w-100"
-            src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide"
-            alt="Third slide"
-          />
+        <div className="carousel-item">
+          <img className="d-block w-100" src={img3} alt="Third slide" />
         </div>
       </div>
       <a
-        class="carousel-control-prev"
+        className="carousel-control-prev"
         href="#carouselExampleIndicators"
         role="button"
         data-slide="prev"
       >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="sr-only">Previous</span>
       </a>
       <a
-        class="carousel-control-next"
+        className="carousel-control-next"
         href="#carouselExampleIndicators"
         role="button"
         data-slide="next"
       >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="sr-only">Next</span>
       </a>
     </div>
   );
