888d937205ccff63e1aa8233584539372059ceab
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-23 19:46:08 -0800

    footer content added

--- frontEnd/src/components/footer.js
+++ frontEnd/src/components/footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+  );
 };
 
 export default Footer;
