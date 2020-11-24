6421fbf86f4ea0db4d99b6ce8dcc101753da913e
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-24 11:44:27 -0800

    refactored footer code

--- frontEnd/src/components/footer.js
+++ frontEnd/src/components/footer.js
@@ -3,21 +3,19 @@ import logo from "../assets/GameBox Logo.png";
 
 const Footer = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container d-flex flex-column">
-        <div className="navbar-brand h1">
-          {" "}
-          <img
-            src={logo}
-            width="25"
-            height="20"
-            className="mr-1 mb-1"
-            alt="Gamebox logo"
-          />
-          Gamebox Center
-        </div>
-        <div className="navbar-brand h5">A Rad Industries Brand</div>
+    <nav className="container-fluid navbar navbar-dark bg-dark flex-column">
+      <div className="navbar-brand">
+        {" "}
+        <img
+          src={logo}
+          width="25"
+          height="20"
+          className="mr-1 mb-1"
+          alt="Gamebox logo"
+        />
+        Gamebox Center
       </div>
+      <div className="navbar-brand">A Rad Industries Brand</div>
     </nav>
   );
 };
