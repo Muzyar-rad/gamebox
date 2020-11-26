df18169f3e10ab22fca1c2ee4dccce0d2c19d688
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-26 11:04:00 -0800

    product images and cards being added

--- frontEnd/src/components/cards.js
+++ frontEnd/src/components/cards.js
@@ -2,13 +2,112 @@ import React from "react";
 
 const Cards = () => {
   return (
-    <div class="card" style={{ width: "18rem" }}>
-      <img class="card-img-top" src="..." alt="product pic" />
-      <div class="card-body">
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+    <div class="card-deck">
+      <div class="card">
+        <img
+          class="card-img-top"
+          src={require("../assets/img/Alienware R10 Desktop.webp")}
+          alt="product_pic_1"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Alienware Aurora R10 Gaming Desktop</h5>
+          <p class="card-text">
+            This is a longer card with supporting text below as a natural
+            lead-in to additional content. This content is a little bit longer.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img
+          class="card-img-top"
+          src={require("../assets/img/Alienware R11 Desktop.webp")}
+          alt="product_pic_2"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Alienware Aurora R11 Gaming Desktop</h5>
+          <p class="card-text">
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img
+          class="card-img-top"
+          src={require("../assets/img/Alienware Area 51m Laptop.jpg")}
+          alt="product_pic_3"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Alienware Area 51 Gaming Laptop</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img
+          class="card-img-top"
+          src={require("../assets/img/Alienware M17 Laptop.webp")}
+          alt="product_pic_3"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Alienware M17 Gaming Laptop</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img
+          class="card-img-top"
+          src={require("../assets/img/Dell G5 Gaming Desktop.webp")}
+          alt="product_pic_3"
+        />
+        <div class="card-body">
+          <h5 class="card-title">Dell G5 Gaming Desktop</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <img
+          class="card-img-top"
+          src={require("../assets/img/corsair vengeance 7200i.png_515Wx515H")}
+          alt="product_pic_3"
+        />
+        <div class="card-body">
+          <h5 class="card-title">CORSAIR Vengeance i7200 Gaming Desktop</h5>
+          <p class="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+          </p>
+          <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
       </div>
     </div>
   );
