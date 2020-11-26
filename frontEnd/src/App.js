97af2d9b1a843b6fecc9f6be2d5cf15ad167105b
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-25 18:08:23 -0800

    successfully added new font-type

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./components/footer";
 import Carousal from "./components/carousal";
 import Header from "./components/header";
+import "./css/App.css";
 
 function App() {
   return (
