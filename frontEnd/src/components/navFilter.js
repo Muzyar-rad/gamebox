a2fc2a1f0b6223d2086e05370bf2cecb6d707d63
Muzyar Rad <muzyar.rad@gmail.com>
2020-11-30 16:02:54 -0800

    navFilter created

--- frontEnd/src/components/navFilter.js
+++ frontEnd/src/components/navFilter.js
@@ -4,16 +4,21 @@ import { Link } from "react-router-dom";
 const NavFilter = () => {
   return (
     <div>
-      <nav className="nav nav-pills nav-justified">
-        <Link className="nav-item nav-link" to="#">
-          DESKTOPS
-        </Link>
-        <Link className="nav-item nav-link" to="#">
-          LAPTOPS
-        </Link>
-        <Link className="nav-item nav-link" to="#">
-          GEARSHOP
-        </Link>
+      <nav className="navbar navbar-dark bg-dark mt-3">
+        <div className="container">
+          <Link className="nav-item nav-link text-light" to="#">
+            <i className="fa fa-desktop mr-1"></i>
+            DESKTOPS
+          </Link>
+          <Link className="nav-item nav-link text-light" to="#">
+            <i className="fa fa-laptop mr-1"></i>
+            LAPTOPS
+          </Link>
+          <Link className="nav-item nav-link text-light" to="#">
+            <i className="fa fa-cogs mr-1"></i>
+            GEARSHOP
+          </Link>
+        </div>
       </nav>
     </div>
   );
