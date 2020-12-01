8f3cb120ffa3a9d83d13a5764336ba89200386b5
Muzyar Rad <muzyar.rad@gmail.com>
2020-12-01 11:53:01 -0800

    almost done with card item

--- frontEnd/src/components/cardItemPage.js
+++ frontEnd/src/components/cardItemPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../css/cardItemPage.css";
 
 const CardItemPage = () => {
   return (
     <div className="container p-5">
       <div className="card">
-        <NavLink className="p-2" to="/">
+        <NavLink className="text-style p-2" to="/">
           <i className="fa fa-chevron-circle-left m-1"></i>
           Back to Catalog
         </NavLink>
@@ -13,7 +14,7 @@ const CardItemPage = () => {
           <div className="row">
             <div className="col">
               <img
-                className="card-img-fit float-left pr-5"
+                className="card-img-fit mx-auto d-block"
                 src={require("../assets/img/Alienware R11 Desktop.webp")}
                 alt="product_pic_2"
               />
@@ -22,22 +23,28 @@ const CardItemPage = () => {
               <h5 className="card-title">
                 Alienware Aurora R11 Gaming Desktop
               </h5>
-              <p className="card-text">
-                <small className="text-muted">$3,659.99</small>
-              </p>
-              <p className="card-text">
+              <p className="card-price text-muted">$3,659.99</p>
+              <p className="card-desc">
                 The Alienware Aurora R11 is engineered with 10th Gen Intel®
                 Core™ processors, optional liquid-cooled graphics and multiple
                 upgrade options to achieve peak performance that lasts.
               </p>
+              <button type="button" className="btn btn-color text-light">
+                Add to Cart
+              </button>
             </div>
           </div>
-          <pre>
-            10th Gen Intel® Core™ i7 10700KF Windows 10 Home NVIDIA® GeForce
-            RTX™ 3080 10GB GDDR6X 16GB Dual Channel HyperX™ FURY DDR4 XMP at
-            2933MHz; up to 64GB 512GB M.2 PCIe NVMe SSD (Boot) + 1TB 7200RPM
-            SATA 6Gb/s (Storage)
-          </pre>
+          <p>&#9642;10th Gen Intel® Core™ i9 10900F</p>
+          <p>&#9642;Windows 10 Home</p>
+          <p>&#9642;NVIDIA® GeForce RTX™ 3080 10GB GDDR6X </p>
+          <p>
+            &#9642;16GB Dual Channel HyperX™ FURY DDR4 XMP at 2933MHz; up to
+            64GB{" "}
+          </p>
+          <p>
+            &#9642;512GB M.2 PCIe NVMe SSD (Boot) + 1TB 7200RPM SATA 6Gb/s
+            (Storage)
+          </p>
         </div>
       </div>
     </div>
