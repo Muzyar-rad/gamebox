e13ca4f628445d5cb2e2f567e1bca3761c8feb1b
Muzyar Rad <muzyar.rad@gmail.com>
2020-12-07 17:45:54 -0800

    cleared the codebase and did some cleanup

--- frontEnd/src/components/navFilter.js
+++ frontEnd/src/components/navFilter.js
@@ -6,15 +6,15 @@ const NavFilter = () => {
     <div>
       <nav className="navbar navbar-dark bg-dark mt-3">
         <div className="container">
-          <NavLink className="nav-item nav-link text-light" to="#">
+          <NavLink className="nav-item nav-link text-light" to="/">
             <i className="fa fa-desktop mr-1"></i>
             DESKTOPS
           </NavLink>
-          <NavLink className="nav-item nav-link text-light" to="#">
+          <NavLink className="nav-item nav-link text-light" to="/">
             <i className="fa fa-laptop mr-1"></i>
             LAPTOPS
           </NavLink>
-          <NavLink className="nav-item nav-link text-light" to="#">
+          <NavLink className="nav-item nav-link text-light" to="/">
             <i className="fa fa-cogs mr-1"></i>
             GEARSHOP
           </NavLink>
