008af355c7c81f74430579691bee444f00358e28
Muzyar Rad <muzyar.rad@gmail.com>
2020-12-11 18:29:02 -0800

    getFilteredData almost functional

--- frontEnd/src/components/cards.js
+++ frontEnd/src/components/cards.js
@@ -3,7 +3,7 @@ import CardItem from "./cardItem";
 import { getProducts } from "../services/productService";
 import "../css/cards.css";
 
-const Cards = () => {
+const Cards = (props) => {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const getProductList = async () => {
@@ -12,13 +12,33 @@ const Cards = () => {
     };
     getProductList();
   }, []);
+
+  const getFilteredData = (products) => {
+    console.log(products);
+    if (props.filter === "")
+      return products.map((product) => (
+        <CardItem key={product.productId} product={product} />
+      ));
+    else if (props.filter === "Desktop") {
+      products.forEach((product) => product.name.match(/(Desktop)/));
+      return products.map((product) => (
+        <CardItem key={product.productId} product={product} />
+      ));
+    } else if (props.filter === "Laptop") {
+      products.forEach((product) => product.name.match(/(Laptop)/));
+      return products.map((product) => (
+        <CardItem key={product.productId} product={product} />
+      ));
+    } else if (props.filter === "Gear") {
+      products.forEach((product) => product.name.match(/(Laptop)/));
+      return products.map((product) => (
+        <CardItem key={product.productId} product={product} />
+      ));
+    }
+  };
   return (
     <React.Fragment>
-      <div className="card-deck p-5">
-        {products.map((product) => (
-          <CardItem key={product.productId} product={product} />
-        ))}
-      </div>
+      <div className="card-deck p-5">{getFilteredData(products)}</div>
     </React.Fragment>
   );
 };
