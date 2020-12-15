a0567595e5ec9aadcf6c6e39619b1d8f33c774a6
Muzyar Rad <muzyar.rad@gmail.com>
2020-12-14 21:17:00 -0800

    will add link to each item next

--- frontEnd/src/components/navFilter.js
+++ frontEnd/src/components/navFilter.js
@@ -3,39 +3,37 @@ import "../css/navFilter.css";
 
 const NavFilter = (props) => {
   return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark mt-3">
-        <div className="container">
-          <div
-            className="nav-item nav-link text-white"
-            onClick={() => {
-              props.onFilterSelect("Desktop");
-            }}
-          >
-            <i className="fa fa-desktop mr-1"></i>
-            DESKTOPS
-          </div>
-          <div
-            className="nav-item nav-link text-white"
-            onClick={() => {
-              props.onFilterSelect("Laptop");
-            }}
-          >
-            <i className="fa fa-laptop mr-1"></i>
-            LAPTOPS
-          </div>
-          <div
-            className="nav-item nav-link text-white"
-            onClick={() => {
-              props.onFilterSelect("Gear");
-            }}
-          >
-            <i className="fa fa-cogs mr-1"></i>
-            GEARSHOP
-          </div>
+    <nav className="navbar navbar-dark bg-dark mt-3">
+      <div className="container">
+        <div
+          className="nav-item nav-link text-white"
+          onClick={() => {
+            props.onFilterSelect("Desktop");
+          }}
+        >
+          <i className="fa fa-desktop mr-1"></i>
+          DESKTOPS
         </div>
-      </nav>
-    </div>
+        <div
+          className="nav-item nav-link text-white"
+          onClick={() => {
+            props.onFilterSelect("Laptop");
+          }}
+        >
+          <i className="fa fa-laptop mr-1"></i>
+          LAPTOPS
+        </div>
+        <div
+          className="nav-item nav-link text-white"
+          onClick={() => {
+            props.onFilterSelect("Gear");
+          }}
+        >
+          <i className="fa fa-cogs mr-1"></i>
+          GEARSHOP
+        </div>
+      </div>
+    </nav>
   );
 };
 
